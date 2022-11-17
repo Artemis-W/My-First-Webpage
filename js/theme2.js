abb5d3d8cf7d9f92c4c4eccf8fd24479eb3010c6
Artemis Wetzler <artemis.wetzler@gmail.com>
2022-11-17 11:22:41 -0300

    17/11/22

--- js/theme2.js
+++ js/theme2.js
@@ -1,11 +1,27 @@
-let darkEnabled = Boolean(localStorage.darkEnabled);
+var themeSwitch = document.getElementById('themeSwitch');
 const cssElem = document.querySelector('link[rel="stylesheet"]');
-if (!darkEnabled) cssElem.href = '../css/light.css';
-else cssElem.href = '../css/dark.css';
-document.getElementById("theme-btn")
-    .addEventListener('click', function () {
-    darkEnabled = !darkEnabled;
-    localStorage.darkEnabled = darkEnabled;
-    if (!darkEnabled) cssElem.href = '../css/light.css';
-    else cssElem.href = '../css/dark.css';
+if(themeSwitch) {
+  initTheme(); // on page load, if user has already selected a specific theme -> apply it
+
+themeSwitch.addEventListener('change', function(event){
+    resetTheme(); // update color theme
 });
+
+function initTheme() {
+    var darkThemeSelected = (localStorage.getItem('themeSwitch') !== null && localStorage.getItem('themeSwitch') === 'dark');
+    // update checkbox
+    themeSwitch.checked = darkThemeSelected;
+    // update body data-theme attribute
+    darkThemeSelected ? cssElem.href = '../css/dark.css': cssElem.href = '../css/light.css'
+};
+
+function resetTheme() {
+    if(themeSwitch.checked) { // dark theme has been selected
+    cssElem.href = '../css/dark.css';
+      localStorage.setItem('themeSwitch', 'dark'); // save theme selection 
+    } else {
+    cssElem.href = '../css/light.css';
+      localStorage.removeItem('themeSwitch'); // reset theme selection 
+    } 
+};
+}
